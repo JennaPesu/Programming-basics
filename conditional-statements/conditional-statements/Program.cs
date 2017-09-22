bf322de12038d2a1becac5e8416ed24557231a45
jenna <jenna.pesu@gmail.com>
2017-09-22 13:10:35 +0300

    Initial commit

---
+++ conditional-statements/conditional-statements/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace conditional_statement
+{
+    class Program
+    {
+        static void Main(string[] args)
+        { 
+            // Define instructions 
+            Console.WriteLine("Ohjelma tulostaa positiivinen, negatiivinen tai nolla");
+            bool isNumber;
+            do
+            { 
+
+
+            Console.Write("Syötä luku: ");
+            string userInput;
+            userInput = Console.ReadLine();
+
+            int evaluetednumber;
+            isNumber = int.TryParse(userInput, out evaluetednumber);
+                // Program logic
+                if (isNumber == true)
+                {
+                    if (evaluetednumber < 0)
+                    {
+                        Console.WriteLine($"Numero {evaluetednumber} on negatiivinen");
+                    }
+                    else if (evaluetednumber > 0)
+                    {
+                        Console.WriteLine($"Numero {evaluetednumber} on negatiivinen");
+                    }
+                    else
+                    {
+                        Console.WriteLine($"Numero {evaluetednumber} on nolla");
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Syötit muuta kun numeron");
+                }
+            } while(isNumber == true);
+            Console.ReadKey();
+            
+             
+         
+        }
+    }
+}
