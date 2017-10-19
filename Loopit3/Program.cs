749871e53a347c2ff813a14a8a9859b1086b5ed0
jenna <jenna.pesu@gmail.com>
2017-10-19 11:14:05 +0300

    New-Loops

--- Loopit3/Program.cs
+++ Loopit3/Program.cs
@@ -9,8 +9,35 @@ namespace Loopit3
     class Program
     {
         static void Main(string[] args)
-        { 
+        {
+            Console.Write("Ohjelma laskee {N} arvon");
+            int luku = 1;
+            {
+                luku = int.Parse(Console.ReadLine());
+                int i = 1;
+                int parillinen = 0;
+                int pariton = 0;
+                while (i <= luku)
+                {
+                    if (i % 2 != 0)
+                    {
+                        pariton = pariton + i;
+                    }
+                    else
+                    {
+                        parillinen = parillinen + i;
+                    }
+                    i++;
+                }
 
+
+                Console.WriteLine($"Parittomien summa = " + pariton);
+                Console.WriteLine($"Parillisten summa =" + parillinen);
+                Console.ReadKey();
+
+
+
+            }
         }
     }
 }
