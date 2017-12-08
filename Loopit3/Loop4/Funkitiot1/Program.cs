a5229f88de773e9bda1e6195abff237d00fba07b
jenna <jenna.pesu@gmail.com>
2017-12-08 11:13:10 +0200

    Funktio 2 valmis

---
+++ Loopit3/Loop4/Funkitiot1/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Funkitiot1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ohjelma palauttaa kahdesta arvosta pienemmän");
+            Console.Write("Syötä 1. luku: ");
+            string userInput = Console.ReadLine();
+            int numberX = int.Parse(userInput);
+
+            Console.Write("Syötä 2. luku: ");
+            userInput = Console.ReadLine();
+            int numberY = int.Parse(userInput);
+
+
+            
+            Console.WriteLine($" pienempi luvuista on {minimi(numberX, numberY)} ");
+            Console.ReadKey(); 
+        }
+
+        static int minimi(int x, int y)
+        {
+            if (x < y)
+                return x;
+            else
+                return y;
+        } 
+    } 
+}
